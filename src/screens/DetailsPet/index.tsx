4d8377a5ea4fad78796719184c0bdb16358d2b8f
Heitor Pires <heitor.pires@consorciomagalu.com.br>
2022-03-19 20:51:49 -0300

    edit style DetailList

--- src/screens/DetailsPet/index.tsx
+++ src/screens/DetailsPet/index.tsx
@@ -8,38 +8,48 @@ import { ButtonIcon } from "../../components/ButtonIcon";
 import { Header } from "../../components/Header";
 
 import results from "../../../results";
+import { styles } from "./styles";
+import { ScrollView } from "react-native-gesture-handler";
 //https://app.rocketseat.com.br/node/mission-react-native/group/nlw-together-react-native/lesson/aula-03-in-orbit-2
 
 export function DetailsPet() {
   const uri = "https://s2.glbimg.com/DVfIiTGl-KnJU41UcD9Yoj33MZM=/e.glbimg.com/og/ed/f/original/2021/06/16/doge.jpg"
   return (
-    <View>
-      <Header />
-      <View>
-        <Text>
 
-        </Text>
-        <View>
-          <Image
-            source={{uri}}
-          />
+    <View >
+      <Header />
+      <View style={styles.container}>
+
+        <View style={styles.card}>
+          <Text style={styles.name}>
+            {"TESTE"}
+          </Text>
           <View>
-            <Text></Text>
-            <Text></Text>
-            <Text></Text>
-            <Text></Text>
+            <Image
+              style={styles.imgPet}
+              source={{ uri }}
+              resizeMode="cover"
+            />
+            <View style={styles.info}>
+              <Text style={styles.infoContent}>{"teste"}</Text>
+              <Text style={styles.infoContent}>Porte: {"teste"}</Text>
+              <Text style={styles.infoContent}>Idade: {"teste"}</Text>
+              <Text style={styles.infoContent}>{"teste"}</Text>
+            </View>
           </View>
-          <View>
-            <Text></Text>
-            <Text></Text>
+          <ScrollView style={styles.descriptionContent}>
+            <Text style={styles.descriptionTitle}>Descrição</Text>
+            <Text style={styles.descriptionText}>{"teste"}</Text>
+          </ScrollView>
+          <View style={styles.buttonContainer}>
+            <ButtonIcon
+              title="Entrar em contato"
+              name={"whatsapp"}
+              size={20}
+              color={"#7CFC00"}
+            />
           </View>
         </View>
-        <ButtonIcon
-          title="Entrar em contato"
-          name={"whatsapp"}
-          size={20}
-          color={"#7CFC00"}
-        />
       </View>
     </View>
   );
--- src/screens/DetailsPet/styles.ts
+++ src/screens/DetailsPet/styles.ts
@@ -1,5 +1,68 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.primaryColorBlack,
+    alignItems: 'center',
+  },
+  card: {
+    height: 614,
+    width: 363,
+    borderRadius: 10,
+    marginTop: 46,
+    marginHorizontal: 6,
+    alignItems: 'center',
+    backgroundColor: theme.colors.cardColorGrey,
+  },
+  name: {
+    fontFamily: theme.fonts.titleMedium,
+    fontSize: 20,
+    color: theme.colors.textColorWhite,
+    marginTop: 39,
+    marginBottom: 19
+  },
+  imgPet: {
+    height: 240,
+    width: 350,
+    borderRadius: 10,
+    
+  },
+  info: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 42,
+    marginBottom: 26,
+  },
+  infoContent: {
+    fontFamily: theme.fonts.textBold,
+    fontSize: 10,
+    color: theme.colors.textColorWhite,
+  },
+  descriptionContent: {
+    width: '100%',
+    maxHeight: 100,
+    marginVertical: 10,
+    textAlign: 'justify',
+  },
+  descriptionTitle: {
+    marginBottom: 10,
+    fontFamily: theme.fonts.titleMedium,
+    fontSize: 12,
+    color: theme.colors.textColorWhite,
+    textAlign: "center"
+  },
+  descriptionText: {
+    fontFamily: theme.fonts.textRegular,
+    fontSize: 10,
+    color: theme.colors.textColorWhite,
+    marginHorizontal: 43.5
+  },
+  buttonContainer: {
+    justifyContent: 'flex-end',
+    marginVertical: 20
+  },
 });
